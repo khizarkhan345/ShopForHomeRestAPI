8456d4f585306c2d2d7ec5c46c57ef354ae05257
Khizar Khan <khezarhayat881@gmail.com>
2024-08-08 17:22:10 -0400

    Creat the project structure

---
+++ src/main/java/com/shopforhome/com/ShopForHomeRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.shopforhome.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopForHomeRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopForHomeRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopforhome/com/ShopForHomeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopforhome.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopForHomeRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
