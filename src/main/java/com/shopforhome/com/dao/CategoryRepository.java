4797d653669883b1489c2eaaf38af718a5aad278
Khizar Khan <khezarhayat881@gmail.com>
2024-08-09 15:49:01 -0400

    Create the Dao Repository for each entity

---
+++ src/main/java/com/shopforhome/com/dao/CategoryRepository.java
@@ -0,0 +1,13 @@
+package com.shopforhome.com.dao;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.shopforhome.com.entity.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<UUID, Category>{
+
+}
